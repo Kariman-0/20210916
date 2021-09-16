a40bba5662112ab13a38f0fa417350c1870dc002
Kariman <csm40718@hanmail.net>
2021-09-16 15:20:22 +0900

    10만 뺌

--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 int main()
 {
-	cout << "*\n";
-	cout << "**\n";
-	cout << "***\n";
-	cout << "****\n";
-	cout << "*****\n";
+	cout << 1 + 1 << endl;
+
+
+	cout << 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 << endl;
 }
