47ffe98b6bef7baad10eee3d0b67d65a8df3c713
Kariman <csm40718@hanmail.net>
2021-09-16 13:17:26 +0900

    ** 4줄

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
 	cout << "*\n";
-	cout << "*\n";
-	cout << "*\n";
-	cout << "*\n";
-	cout << "*\n";
+	cout << "**\n";
+	cout << "**\n";
+	cout << "**\n";
+	cout << "**\n";
 }
